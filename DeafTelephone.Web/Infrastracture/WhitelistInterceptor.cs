fcd578d2a81d9f5a2839b6788cc3a37d21c959bc
kitakun <xfikysx@ya.ru>
2021-08-18 16:54:16 +0300

    1.0.2  * add whitelist access by api key

--- DeafTelephone.Web/Infrastracture/WhitelistInterceptor.cs
+++ DeafTelephone.Web/Infrastracture/WhitelistInterceptor.cs
@@ -5,6 +5,8 @@
     using Grpc.Core;
     using Grpc.Core.Interceptors;
 
+    using Microsoft.Extensions.Caching.Memory;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Logging;
 
     using System.Security;
@@ -14,13 +16,22 @@
     {
         private readonly IWhitelistService _whitelistService;
         private readonly ILogger<WhitelistInterceptor> _logger;
+        private readonly IConfiguration _appConfiguration;
+        private readonly IMemoryCache _memoryCache;
+        private static readonly object mem_api_key = new();
+
+        private const string WHITELIST_API_KEY = "whitelist_api_key";
 
         public WhitelistInterceptor(
             IWhitelistService whitelistService,
-            ILogger<WhitelistInterceptor> logger)
+            ILogger<WhitelistInterceptor> logger,
+            IConfiguration appConfiguration,
+            IMemoryCache memoryCache)
         {
             _whitelistService = whitelistService ?? throw new System.ArgumentNullException(nameof(whitelistService));
             _logger = logger ?? throw new System.ArgumentNullException(nameof(logger));
+            _appConfiguration = appConfiguration ?? throw new System.ArgumentNullException(nameof(appConfiguration));
+            _memoryCache = memoryCache ?? throw new System.ArgumentNullException(nameof(memoryCache));
         }
 
         public override async Task<TResponse> UnaryServerHandler<TRequest, TResponse>(
@@ -28,6 +39,24 @@
             ServerCallContext context,
             UnaryServerMethod<TRequest, TResponse> continuation)
         {
+            // validate access by API Key
+            var apiAccess = context.RequestHeaders.GetValue(WHITELIST_API_KEY);
+            if (!string.IsNullOrEmpty(apiAccess))
+            {
+                var keyFromConfig = _memoryCache.GetOrCreate(mem_api_key, (entry) =>
+                {
+                    return _appConfiguration["WhitelistKey"];
+                });
+
+                if (!string.IsNullOrEmpty(keyFromConfig) && keyFromConfig.Equals(apiAccess, System.StringComparison.OrdinalIgnoreCase))
+                {
+                    return await continuation(request, context);
+                }
+            }
+
+            context.CancellationToken.ThrowIfCancellationRequested();
+
+            // validate access by WhiteList API list
             var httpContext = context.GetHttpContext();
             var clientIP = httpContext.Connection.RemoteIpAddress;
             var address = clientIP.ToString();
@@ -41,6 +70,8 @@
             var isAllowed = await _whitelistService.IsAllowedAsync(address);
             if (isAllowed)
             {
+                context.CancellationToken.ThrowIfCancellationRequested();
+
                 return await continuation(request, context);
             }
 
