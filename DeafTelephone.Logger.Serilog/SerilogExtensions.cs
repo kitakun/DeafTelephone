6cf87c9d6487abf3aac1551dc99bddcf7d528dd2
kitakun <xfikysx@ya.ru>
2021-11-16 21:46:18 +0300

    * rise to NET 6

--- DeafTelephone.Logger.Serilog/SerilogExtensions.cs
+++ DeafTelephone.Logger.Serilog/SerilogExtensions.cs
@@ -6,21 +6,32 @@
 
     public static class SerilogExtensions
     {
+        /// <summary>
+        /// Globally create a logger and return the instance
+        /// </summary>
         public static ILogger CreateLogger()
         {
             Log.Logger = new LoggerConfiguration()
                 .WriteTo.Console()
+#if RELEASE
                 .WriteTo.File("log-.txt", rollingInterval: RollingInterval.Day)
+#endif
                 .CreateLogger();
 
             return Log.Logger;
         }
 
+        /// <summary>
+        /// Close logger
+        /// </summary>
         public static void Dispose()
         {
             Log.CloseAndFlush();
         }
 
+        /// <summary>
+        /// Inject the logger into ASP.NET environment
+        /// </summary>
         public static IHostBuilder ApplySerilogToTheProject(this IHostBuilder hostBuilder)
         {
             return hostBuilder.UseSerilog();
